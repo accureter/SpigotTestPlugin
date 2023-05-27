3eef20cb39af52d6706666e3f57980658704cda6
accureter <accureter@gmail.com>
2023-05-28 00:13:46 +0200

    Commands Part 1

--- src/main/java/pl/accureter/thirdfirstplugin/ThirdFirstPlugin.java
+++ src/main/java/pl/accureter/thirdfirstplugin/ThirdFirstPlugin.java
@@ -1,6 +1,12 @@
 package pl.accureter.thirdfirstplugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.BlockCommandSender;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ThirdFirstPlugin extends JavaPlugin {
@@ -14,6 +20,22 @@ public final class ThirdFirstPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        Bukkit.getLogger().info("Zawijam mandżur");
+        Bukkit.getLogger().info("Zawijam mandzur");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(command.getName().equalsIgnoreCase("die")){
+            if (sender instanceof Player player){
+                player.setHealth(0.0);
+                player.sendMessage(ChatColor.RED + "Debilu zabiłeś się XD");
+            } else if (sender instanceof ConsoleCommandSender) {
+                Bukkit.getLogger().warning("This command is only for Player (sent by Console)");
+            } else if (sender instanceof BlockCommandSender) {
+                Bukkit.getLogger().warning("This command is only for Player (sent by CommandBlock)");
+            }
+        }
+
+        return true;
     }
 }
