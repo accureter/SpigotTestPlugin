356d1d1c3b741b3196e036bb8c0d334b999899fa
accureter <accureter@gmail.com>
2023-05-28 13:42:12 +0200

    Permissions

--- src/main/java/pl/accureter/thirdfirstplugin/commands/FeedCommand.java
+++ src/main/java/pl/accureter/thirdfirstplugin/commands/FeedCommand.java
@@ -1,5 +1,6 @@
 package pl.accureter.thirdfirstplugin.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,8 +12,12 @@ public class FeedCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (sender instanceof Player player){
-            player.setFoodLevel(20);
-            player.sendMessage("Nakarmiono Cię :D");
+            if (player.hasPermission("thirdfirstplugin.feed")) {
+                player.setFoodLevel(20);
+                player.sendMessage("Nakarmiono Cię :D");
+            }else {
+                player.sendMessage(ChatColor.RED + "Nie masz wymaganej permisji (thirdfirstplugin.feed).");
+            }
         }
 
         return true;
---
+++ src/main/java/pl/accureter/thirdfirstplugin/events/PlayerMove.java
@@ -0,0 +1,17 @@
+package pl.accureter.thirdfirstplugin.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class PlayerMove implements Listener {
+
+    @EventHandler
+    void onPlayerMove(PlayerMoveEvent event){
+        Player player = event.getPlayer();
+        if (!player.hasPermission("thirdfirstplugin.move")){
+            event.setCancelled(true);
+        }
+    }
+}
